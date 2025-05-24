8728dd28686cbef199e11c8b2845e36ba03d5f69
Josephine Bhadran <josephinebhadran05@gmail.com>
2025-05-24 09:10:08 -0700

    she's alive!

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+# app.register_blueprint(auth_routes, url_prefix="/auth")
+
+@app.route('/')
+def home():
+    return {"message": "Welcome to the Study Pod API!"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
