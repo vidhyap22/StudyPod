9de7481ed9171b77b293bddbaae44a1ecf766344
vidhya <vapillai@uci.edu>
2025-05-24 14:49:01 -0700

    add create task

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from models.user import auth_routes
 from models.project import project_routes
+from models.task import task_routes
 
 app = Flask(__name__)
 
 app.register_blueprint(auth_routes, url_prefix="/auth")
 app.register_blueprint(project_routes, url_prefix="/project")
+app.register_blueprint(task_routes, url_prefix="/task")
 
 @app.route('/')
 def home():
--- backend/models/task.py
+++ backend/models/task.py
@@ -1,5 +1,8 @@
 import sqlite3
+from flask import Flask, request, jsonify, Blueprint, make_response
+from models.user import token_required 
 
+task_routes = Blueprint("task_routes", __name__)
 
 def create_project_table():
     """This is creating the whole table where all the projects will go."""
@@ -14,11 +17,34 @@ def create_project_table():
         is_completed BOOLEAN DEFAULT 0,
         FOREIGN KEY (user_id) REFERENCES User(user_id),
         FOREIGN KEY (project_id) REFERENCES Project(project_id)
-    );
+    )
     """)
 
     conn.commit()
     conn.close()
 
-create_project_table()
+# create_project_table()
+
+
+@task_routes.route('/create-task', methods=['POST'])
+@token_required
+def create_task(current_user):
+
+    data = request.json
+
+    try:
+        conn = sqlite3.connect("gus.db")
+        cur = conn.cursor()
+
+        cur.execute("""INSERT INTO Task (task_name, user_id, project_id, is_completed) 
+                    VALUES (?, ?, ?, ?)""", 
+                    (data["task_name"], data["user_id"], data["project_id"], data["is_completed"]))
+        conn.commit()
+
+        return jsonify(msg="Created task!"), 201
+    except sqlite3.IntegrityError as e:
+        return jsonify(msg=f"Error creating task: {str(e)}"), 500
+    finally:
+        conn.close()
+
 
