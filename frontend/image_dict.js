519633c8cd9904f36707ec6e47f9021720582a2e
Viviana Salazar <vivianasalazar626@gmail.com>
2025-05-24 23:14:48 -0700

    Styling and Image elements

---
+++ frontend/image_dict.js
@@ -0,0 +1,4 @@
+let level_gus_image = {};
+
+//level_gus_image[3] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_1.PNG"
+//level_gus_image[3] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_1.PNG"
