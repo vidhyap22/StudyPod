e9009560f0264595f5698af7b2f4f14f2602ec94
Viviana Salazar <vivianasalazar626@gmail.com>
2025-05-25 01:23:03 -0700

    Added level image support

--- frontend/image_dict.js
+++ frontend/image_dict.js
@@ -1,4 +1,7 @@
 let level_gus_image = {};
 
-//level_gus_image[3] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_1.PNG"
-//level_gus_image[3] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_1.PNG"
+level_gus_image[3] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_1.PNG"
+level_gus_image[6] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_2.PNG"
+level_gus_image[9] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_3.PNG"
+level_gus_image[12] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_4.PNG"
+level_gus_image[15] = "C:\Users\vivia\OneDrive\Documents\GitHub\StudyPod\frontend\images\gus_level_5.PNG"
